72d376c172c63ce2f2a6dd6e97e82983eec0d712
umesh-kushwaha <umesh220388@gmail.com>
2018-09-24 23:12:12 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes} from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material';
+import {HttpModule} from "@angular/http";
+import {MatInputModule} from '@angular/material';
+
+
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { UserloginComponent } from './userlogin/userlogin.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+
+
+const appRoutes: Routes = [
+   {
+      path: '',
+      component: UserloginComponent
+   },
+   {
+      path: 'app-mainpage',
+      component: MainpageComponent
+   }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    UserloginComponent,
+    MainpageComponent
   ],
   imports: [
-    BrowserModule
+      BrowserModule,
+      ReactiveFormsModule,
+      RouterModule.forRoot(appRoutes),
+      BrowserAnimationsModule,
+      HttpModule,
+      MatTableModule,
+      MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
